64012526937a319488a0994eddcf9d60208e3e5c
rayyanabubakr <71322861+rayyanabubakr@users.noreply.github.com>
2021-09-01 18:38:23 +0530

    Create Coin Change Permutations.cpp

---
+++ Coin Change Permutations.cpp
@@ -0,0 +1,53 @@
+// 1. You are given a number n, representing the count of coins.
+// 2. You are given n numbers, representing the denominations of n coins.
+// 3. You are given a number "amt".
+// 4. You are required to calculate and print the number of permutations of the n coins using which the 
+//      amount "amt" can be paid.
+
+// Note1 -> You have an infinite supply of each coin denomination i.e. same coin denomination can be 
+//                   used for many installments in payment of "amt"
+// Note2 -> You are required to find the count of permutations and not combinations i.e.
+//                   2 + 2 + 3 = 7 and 2 + 3 + 2 = 7 and 3 + 2 + 2 = 7 are different permutations of same 
+//                   combination. You should treat them as 3 and not 1.
+
+#include<iostream>
+#include<string>
+#include <bits/stdc++.h>
+using namespace std;
+
+int targetSubsets(int arr[], int tar, int n)
+{
+  int dp[tar + 1];
+  dp[0] = 1;
+  for (int i = 1 ; i <= tar; i++)
+  {
+    dp[i] = 0;
+  }
+  for (int j = 0; j <= tar  ; j++)
+  {
+    for (int i = 0; i < n; i++)
+    {
+      if (j >= arr[i])
+      {
+
+
+        dp[j] += dp[j - arr[i]];
+      }
+    }
+  }
+
+  return dp[tar];
+}
+int main()
+{
+  int n;
+  cin >> n;
+  int arr[n];
+  for (int i = 0; i < n; i++)
+  {
+    cin >> arr[i];
+  }
+  int target;
+  cin >> target;
+  cout << targetSubsets(arr, target, n);
+}
